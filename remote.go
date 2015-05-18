58d1c0d970303d2b645a5b040bc2c5cd9d0af982
Marcell Jusztin <jusztin.marcell@gmail.com>
2015-05-18 13:34:03 +0100

    more remote functionality

--- remote.go
+++ remote.go
@@ -3,12 +3,43 @@ package got
 import (
 	"fmt"
 	"os/exec"
+	"strings"
 )
 
 func (this *GotClient) SetRemote(name, uri string) error {
 	this.changeDirToPath()
 
 	cmd := exec.Command("git", "remote", "set-url", name, uri)
+	out, err := cmd.CombinedOutput()
+
+	if this.Verbose {
+		fmt.Println(string(out))
+	}
+
+	this.restorePath()
+
+	return err
+}
+
+func (this *GotClient) GetRemote(name string) (string, error) {
+	this.changeDirToPath()
+
+	cmd := exec.Command("git", "config", "--get", fmt.Sprintf("remote.%s.url", name))
+	out, err := cmd.CombinedOutput()
+
+	if this.Verbose {
+		fmt.Println(string(out))
+	}
+
+	this.restorePath()
+
+	return strings.TrimSpace(string(out)), err
+}
+
+func (this *GotClient) AddRemote(name, uri string) error {
+	this.changeDirToPath()
+
+	cmd := exec.Command("git", "remote", "add", name, uri)
 	out, err := cmd.CombinedOutput()
 
 	if this.Verbose {
--- remote_test.go
+++ remote_test.go
@@ -1,38 +1,32 @@
 package got
 
 import (
-	"os/exec"
 	"testing"
 )
 
-func TestSetRemote(t *testing.T) {
+func TestRemote(t *testing.T) {
 	gc, path := setup()
 	defer teardown(path)
 
 	gc.Init()
 
-	gc.changeDirToPath()
-	cmd := exec.Command("git", "remote", "add", "origin", "git@example.com:user/repo1")
-	_, err := cmd.CombinedOutput()
-	gc.restorePath()
+	err := gc.AddRemote("origin", "git@example.com:user/repo1")
 	if err != nil {
 		t.Fatalf("Unexpected error: %s", err)
 	}
 
-	gc.SetRemote("origin", "git@example.com:user/repo2")
-
-	gc.changeDirToPath()
-	cmd = exec.Command("git", "remote", "-v")
-	out, err := cmd.CombinedOutput()
-	gc.restorePath()
-
+	expected := "git@example.com:user/repo2"
+	err = gc.SetRemote("origin", expected)
 	if err != nil {
 		t.Fatalf("Unexpected error: %s", err)
 	}
 
-	expected := "origin\tgit@example.com:user/repo2 (fetch)\n" +
-		"origin\tgit@example.com:user/repo2 (push)\n"
-	if string(out) != expected {
-		t.Errorf("Was expecting:\n%s\ngot:\n%s", expected, out)
+	rem, err := gc.GetRemote("origin")
+	if err != nil {
+		t.Fatalf("Unexpected error: %s", err)
+	}
+
+	if rem != expected {
+		t.Errorf("Was expecting:\n%s\ngot:\n%s", expected, rem)
 	}
 }
